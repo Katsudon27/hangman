a04d2d6dfac1aebf81e234230631856c10b4cd40
Katsudon27 <katsudon1127@gmail.com>
2026-05-13 11:45:05 +1000

    Add methods for core game logic
    Add methods for evaluating user's guess + game loop to start the game

--- lib/game_controller.rb
+++ lib/game_controller.rb
@@ -4,13 +4,29 @@ require_relative "player"
 class GameController
   attr_reader :answer
   def initialize
-    @answer = File.readlines("dictionary.txt").select {|word| word.length >= 5 && word.length <= 12}.sample
+    @answer = File.readlines("dictionary.txt").select {|word| word.length >= 5 && word.length <= 12}.sample.chomp
     @game_board = GameBoard.new(@answer)
     @player = Player.new
   end
 
   def check_win?
     @game_board.guess == @answer.chars
+  end
+
+  def evaluate_guess(guess)
+    if @answer.include?(guess) && guess.length == 1
+      @answer.chars.each_with_index do |letter, index|
+        if letter == guess
+          @game_board.add_letter_guess(index, letter)
+        end
+      end
+    else
+      unless guess == @answer
+        @game_board.add_incorrect_guess(guess)
+        return
+      end
+      @game_board.add_correct_guess(guess)
+    end
   end
 
   def print_instructions
@@ -21,5 +37,26 @@ class GameController
     puts "3. Each incorrect guess will result in a part of the hangman being drawn."
     puts "4. You lose when the hangman is fully drawn."
     puts "START GAME"
+  end
+
+  def start_game
+    print_instructions
+
+    loop do
+      @game_board.print_board
+      guess = @player.make_guess
+      evaluate_guess(guess)
+
+      if check_win?
+        @game_board.print_board
+        puts "\nCongratulations! You have won the game!"
+        break
+      elsif @game_board.incorrect_guesses.length == 7
+        @game_board.print_board
+        puts "\nOops, you have run out out of guesses."
+        puts "The secret word was '#{@answer}'."
+        break
+      end
+    end
   end
 end
