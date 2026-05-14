3e745d8daa373a57d3e0320a7f7097e559cddec3
Katsudon27 <katsudon1127@gmail.com>
2026-05-14 11:38:57 +1000

    Add methods to load previous game save
    Add methods to parse game data for game_controller and game_board + retrieve user input for loading a save

--- lib/game_controller.rb
+++ lib/game_controller.rb
@@ -33,16 +33,37 @@ class GameController
 
   def print_instructions
     puts "Welcome to Hangman!"
+    puts "---------------------------------------"
     puts "Instructions: "
     puts "1. Correctly guess the secret word within 7 guesses."
     puts "2. You can either guess a letter at a time or the entire word if you're confident."
     puts "3. Each incorrect guess will result in a part of the hangman being drawn."
     puts "4. You lose when the hangman is fully drawn."
-    puts "START GAME"
+    puts "5. You can opt to save your current game by typing in 's' as your guess."
+    puts "---------------------------------------"
+  end
+
+  def start_or_load_game
+    puts "[1] Start a new game"
+    puts "[2] Load a previous save"
+
+    loop do
+      puts "Please choose 1 or 2 from the options provided: "
+      input = gets.chomp
+
+      return input if input == "1" or input == "2"
+
+      puts "Invalid input: Please try again"
+    end
   end
 
   def start_game
     print_instructions
+    player_selection = start_or_load_game
+
+    if player_selection == "2"
+      load_game  
+    end
 
     loop do
       @game_board.print_board
@@ -68,11 +89,26 @@ class GameController
       :game_board => @game_board.to_yaml
     })
   end
+
+  def from_yaml(string)
+    data = YAML.load(string)
+    @answer = data[:answer]
+    @game_board = GameBoard.from_yaml(data[:game_board])
+  end
   
   def save_game
     File.open("player_save.yaml", "w") do |f|
       f.write(to_yaml)
     end
     puts "Game successfully saved! You can quit the game now."
+  end
+
+  def load_game
+    if File.exist?("player_save.yaml")
+      from_yaml(File.read("player_save.yaml"))
+      puts "Your previous save from #{File.mtime("player_save.yaml")} has been successfully loaded. Good luck!"
+    else
+      puts "No previous save found. Starting a new game for you!"
+    end
   end
 end
